bbf66454590dd148f0633a6324decd92f36fe10b
Gregory Scott Self <46310761+GregoryScottSelf@users.noreply.github.com>
2019-01-14 21:15:22 -0600

    Add files via upload

---
+++ ChatClient/Form1.Designer.cs
@@ -0,0 +1,221 @@
+﻿namespace ChatClient
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox10 = new System.Windows.Forms.TextBox();
+            this.textBox18 = new System.Windows.Forms.TextBox();
+            this.textBox17 = new System.Windows.Forms.TextBox();
+            this.textBox16 = new System.Windows.Forms.TextBox();
+            this.textBox15 = new System.Windows.Forms.TextBox();
+            this.textBox14 = new System.Windows.Forms.TextBox();
+            this.textBox13 = new System.Windows.Forms.TextBox();
+            this.textBox12 = new System.Windows.Forms.TextBox();
+            this.textBox11 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(60, 15);
+            this.textBox3.Margin = new System.Windows.Forms.Padding(2);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(200, 20);
+            this.textBox3.TabIndex = 2;
+            this.textBox3.TextChanged += new System.EventHandler(this.textBox3_TextChanged);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(278, 108);
+            this.button1.Margin = new System.Windows.Forms.Padding(2);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(56, 50);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Send";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(278, 15);
+            this.button2.Margin = new System.Windows.Forms.Padding(2);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(134, 26);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "Connect to the Server";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(11, 15);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Name";
+            // 
+            // textBox10
+            // 
+            this.textBox10.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.textBox10.Location = new System.Drawing.Point(60, 73);
+            this.textBox10.Margin = new System.Windows.Forms.Padding(2);
+            this.textBox10.Name = "textBox10";
+            this.textBox10.Size = new System.Drawing.Size(32, 20);
+            this.textBox10.TabIndex = 1;
+            this.textBox10.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.textBox10.TextChanged += new System.EventHandler(this.textBox10_TextChanged);
+            // 
+            // textBox18
+            // 
+            this.textBox18.Location = new System.Drawing.Point(193, 181);
+            this.textBox18.Margin = new System.Windows.Forms.Padding(2);
+            this.textBox18.Name = "textBox18";
+            this.textBox18.Size = new System.Drawing.Size(32, 20);
+            this.textBox18.TabIndex = 8;
+            this.textBox18.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.textBox18.TextChanged += new System.EventHandler(this.textBox18_TextChanged);
+            // 
+            // textBox17
+            // 
+            this.textBox17.Location = new System.Drawing.Point(128, 181);
+            this.textBox17.Margin = new System.Windows.Forms.Padding(2);
+            this.textBox17.Name = "textBox17";
+            this.textBox17.Size = new System.Drawing.Size(32, 20);
+            this.textBox17.TabIndex = 9;
+            this.textBox17.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.textBox17.TextChanged += new System.EventHandler(this.textBox17_TextChanged);
+            // 
+            // textBox16
+            // 
+            this.textBox16.Location = new System.Drawing.Point(60, 181);
+            this.textBox16.Margin = new System.Windows.Forms.Padding(2);
+            this.textBox16.Name = "textBox16";
+            this.textBox16.Size = new System.Drawing.Size(32, 20);
+            this.textBox16.TabIndex = 10;
+            this.textBox16.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.textBox16.TextChanged += new System.EventHandler(this.textBox16_TextChanged);
+            // 
+            // textBox15
+            // 
+            this.textBox15.Location = new System.Drawing.Point(193, 124);
+            this.textBox15.Margin = new System.Windows.Forms.Padding(2);
+            this.textBox15.Name = "textBox15";
+            this.textBox15.Size = new System.Drawing.Size(32, 20);
+            this.textBox15.TabIndex = 11;
+            this.textBox15.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.textBox15.TextChanged += new System.EventHandler(this.textBox15_TextChanged);
+            // 
+            // textBox14
+            // 
+            this.textBox14.Location = new System.Drawing.Point(128, 124);
+            this.textBox14.Margin = new System.Windows.Forms.Padding(2);
+            this.textBox14.Name = "textBox14";
+            this.textBox14.Size = new System.Drawing.Size(32, 20);
+            this.textBox14.TabIndex = 12;
+            this.textBox14.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.textBox14.TextChanged += new System.EventHandler(this.textBox14_TextChanged);
+            // 
+            // textBox13
+            // 
+            this.textBox13.Location = new System.Drawing.Point(60, 124);
+            this.textBox13.Margin = new System.Windows.Forms.Padding(2);
+            this.textBox13.Name = "textBox13";
+            this.textBox13.Size = new System.Drawing.Size(32, 20);
+            this.textBox13.TabIndex = 13;
+            this.textBox13.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.textBox13.TextChanged += new System.EventHandler(this.textBox13_TextChanged);
+            // 
+            // textBox12
+            // 
+            this.textBox12.Location = new System.Drawing.Point(193, 73);
+            this.textBox12.Margin = new System.Windows.Forms.Padding(2);
+            this.textBox12.Name = "textBox12";
+            this.textBox12.Size = new System.Drawing.Size(32, 20);
+            this.textBox12.TabIndex = 14;
+            this.textBox12.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.textBox12.TextChanged += new System.EventHandler(this.textBox12_TextChanged);
+            // 
+            // textBox11
+            // 
+            this.textBox11.Location = new System.Drawing.Point(128, 73);
+            this.textBox11.Margin = new System.Windows.Forms.Padding(2);
+            this.textBox11.Name = "textBox11";
+            this.textBox11.Size = new System.Drawing.Size(32, 20);
+            this.textBox11.TabIndex = 15;
+            this.textBox11.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.textBox11.TextChanged += new System.EventHandler(this.textBox11_TextChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(454, 271);
+            this.Controls.Add(this.textBox11);
+            this.Controls.Add(this.textBox12);
+            this.Controls.Add(this.textBox13);
+            this.Controls.Add(this.textBox14);
+            this.Controls.Add(this.textBox15);
+            this.Controls.Add(this.textBox16);
+            this.Controls.Add(this.textBox17);
+            this.Controls.Add(this.textBox18);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox10);
+            this.Margin = new System.Windows.Forms.Padding(2);
+            this.Name = "Form1";
+            this.Text = "TCP Chat Client";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox10;
+        private System.Windows.Forms.TextBox textBox18;
+        private System.Windows.Forms.TextBox textBox17;
+        private System.Windows.Forms.TextBox textBox16;
+        private System.Windows.Forms.TextBox textBox15;
+        private System.Windows.Forms.TextBox textBox14;
+        private System.Windows.Forms.TextBox textBox13;
+        private System.Windows.Forms.TextBox textBox12;
+        private System.Windows.Forms.TextBox textBox11;
+    }
+}
+
---
+++ ChatClient/Form1.cs
@@ -0,0 +1,273 @@
+﻿using System;
+using System.Windows.Forms;
+using System.Text;
+using System.Net.Sockets;
+using System.Threading;
+using System.IO;
+
+namespace ChatClient
+{
+    public partial class Form1 : Form
+    {
+        //creates an instance where the client is able to send information to our server
+        System.Net.Sockets.TcpClient clientSocket = new System.Net.Sockets.TcpClient();
+        NetworkStream serverStream = default(NetworkStream);
+        string readData = null;
+
+
+        //display the game form
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string[] text = {"0", "0", "0", "0","0", "0","0", "0","0" };
+            
+            //if ((textBox10.Text=="x"||textBox10.Text=="o")&& textBox12.Text==")
+
+            text[0] = textBox10.Text;//determines what is sent in the array to the server for each box
+            text[1] = textBox11.Text;
+            text[2] = textBox12.Text;
+            text[3] = textBox13.Text;
+            text[4] = textBox14.Text;
+            text[5] = textBox15.Text;
+            text[6] = textBox16.Text;
+            text[7] = textBox17.Text;
+            text[8] = textBox18.Text;
+            //reads array and sets blank spots to '0'
+            for (int i = 0; i < 9; i++)
+            {
+                if (text[i] == "")
+                {
+                    text[i] = "0";
+                }
+               
+                
+            }
+
+            //sends each textbox to the server with the contents that are included in the array
+            byte[] outStream = System.Text.Encoding.ASCII.GetBytes(text[0]+text[1] + text[2] + text[3] + text[4] + text[5] + text[6] + text[7] + text[8] + "$");
+            
+            serverStream = clientSocket.GetStream();
+            serverStream.Write(outStream, 0, outStream.Length);
+            serverStream.Flush();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            //readData = "Conected to Chat Server ...";
+            //msg();
+            //initialize the instance
+            clientSocket.Connect("127.0.0.1", 8888);
+            serverStream = clientSocket.GetStream();
+            // sends out the users names to the server
+            byte[] outStream = System.Text.Encoding.ASCII.GetBytes(textBox3.Text + "$");
+            //writes out the users name in the server console 
+            serverStream.Write(outStream, 0, outStream.Length);
+            serverStream.Flush();
+
+            Thread ctThread = new Thread(getMessage);
+            ctThread.Start();
+        }
+
+        private void getMessage()
+        {
+            while (true)
+            {
+
+
+                serverStream = clientSocket.GetStream();
+                int buffSize = 0;
+                byte[] inStream = new byte[(int)clientSocket.ReceiveBufferSize];
+                buffSize = clientSocket.ReceiveBufferSize;
+                serverStream.Read(inStream, 0, buffSize);
+                string returndata = System.Text.Encoding.ASCII.GetString(inStream);
+                string board = returndata;
+                //this displays a new window box for who wins 
+                if ((board[0] == 'X' || board[0] == 'O') && board[2] == 'w' && board[3] == 'i' && board[4] == 'n' && board[5] == 's')
+                {
+                    MessageBox.Show(board, "Tic-Tac-Toe", MessageBoxButtons.OK, MessageBoxIcon.Asterisk);
+                    readData = "" + returndata;
+                    
+                }
+                //tie game displayed on new window 
+                else if (board[0]=='C'&&board[1]=='a'&&board[2]=='t')
+                {
+                    MessageBox.Show(board, "Tic-Tac-Toe", MessageBoxButtons.OK, MessageBoxIcon.Asterisk);
+                    readData = "" + returndata;
+                }
+                //no win or loss yet game keeps going 
+                else
+                {
+                    readData = "" + returndata;
+                    msg();
+                }
+            }
+        }
+
+        private void msg()
+        {
+            string board = "0";
+            //string board10 = "0", board11 = "0", board12 = "0", board13 = "0", board14 = "0", board15 = "0", board16 = "0", board17 = "0", board18 = "0";
+            //char boardarr;
+            if (this.InvokeRequired)
+                this.Invoke(new MethodInvoker(msg));
+            else
+            {
+                board = readData;
+                // the box is left empty if the array has the prestored value of '0'
+                //if the user enters in either X or O then it leaves it the same and updates the array that is send to the server
+                if (board[0] != '0')
+                {
+                    textBox10.Text = board[0].ToString();
+                }
+                else
+                {
+                    textBox10.Text = "";
+                }
+
+                if (board[1] != '0')
+                {
+                    textBox11.Text = board[1].ToString();
+                }
+                else
+                {
+                    textBox11.Text = "";
+                }
+
+                if (board[2] != '0')
+                {
+                    textBox12.Text = board[2].ToString();
+                }
+                else
+                {
+                    textBox12.Text = "";
+                }
+
+                if (board[3] != '0')
+                {
+                    textBox13.Text = board[3].ToString();
+                }
+                else
+                {
+                    textBox13.Text = "";
+                }
+
+                if (board[4] != '0')
+                {
+                    textBox14.Text = board[4].ToString();
+                }
+                else
+                {
+                    textBox14.Text = "";
+                }
+
+                if (board[5] != '0')
+                {
+                    textBox15.Text = board[5].ToString();
+                }
+                else
+                {
+                    textBox15.Text = "";
+                }
+
+                if (board[6] != '0')
+                {
+                    textBox16.Text = board[6].ToString();
+                }
+                else
+                {
+                    textBox16.Text = "";
+                }
+
+                if (board[7] != '0')
+                {
+                    textBox17.Text = board[7].ToString();
+                }
+                else
+                {
+                    textBox17.Text = "";
+                }
+                if (board[8] != '0')
+                {
+                    textBox18.Text = board[8].ToString();
+                }
+                else
+                {
+                    textBox18.Text = "";
+                }
+
+            }
+            
+            
+
+
+        }
+        //all the next lines are each individual button and how it sends the array to the server
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+            button2_Click(sender, e);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBoxLoc_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox14_TextChanged(object sender, EventArgs e)
+        {
+            button1_Click(sender, e);
+        }
+
+        private void textBox10_TextChanged(object sender, EventArgs e)
+        {
+            button1_Click(sender, e);
+        }
+        private void textBox11_TextChanged(object sender, EventArgs e)
+        {
+            button1_Click(sender, e);
+        }
+        private void textBox12_TextChanged(object sender, EventArgs e)
+        {
+            button1_Click(sender, e);
+        }
+        private void textBox13_TextChanged(object sender, EventArgs e)
+        {
+            button1_Click(sender, e);
+        }
+        private void textBox15_TextChanged(object sender, EventArgs e)
+        {
+            button1_Click(sender, e);
+        }
+        private void textBox16_TextChanged(object sender, EventArgs e)
+        {
+            button1_Click(sender, e);
+        }
+        private void textBox17_TextChanged(object sender, EventArgs e)
+        {
+            button1_Click(sender, e);
+        }
+        private void textBox18_TextChanged(object sender, EventArgs e)
+        {
+            button1_Click(sender, e);
+        }
+       
+    }
+}
